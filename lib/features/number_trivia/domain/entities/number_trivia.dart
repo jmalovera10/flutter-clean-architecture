b1689b64095cd4ce3d6aa852c0e31eaf74193619
Juan Manuel Lovera <jma.lovera10@uniandes.edu.co>
2021-09-22 09:07:18 -0500

    Part 2: initialize number trivia entity and declare domain repository

---
+++ lib/features/number_trivia/domain/entities/number_trivia.dart
@@ -0,0 +1,12 @@
+import 'package:equatable/equatable.dart';
+
+class NumberTrivia extends Equatable {
+  final String text;
+  final int number;
+
+  const NumberTrivia({required this.text, required this.number})
+      : super();
+
+  @override
+  List<Object?> get props => [text, number];
+}
